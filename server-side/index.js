5cdcad36ad6a9db40b304f04e89097ac1208b1be
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-28 00:01:57 +0600

    server side data send to database done

--- server-side/index.js
+++ server-side/index.js
@@ -10,11 +10,13 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mx4ls.mongodb.net/?appName=Cluster0`;
+
+
+
+
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,12 +31,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const UserDB = client.db("PracticeTwo").collection("Users");
+
+
+    app.post("/users", async(req, res)=> {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await UserDB.insertOne(newUser);
+      res.send(result); 
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
